1f08ec69cbdac2f833669da06b16b8f0e424e83f
Alen <ismalen321@gmail.com>
2025-02-06 15:40:48 -0500

    Added environment and raining sounds inside/outside the house; added sounds for taking pills, doors and side sound.

---
+++ Assets/Scripts/DoorSoundScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using FMODUnity;
+
+public class DoorSound : MonoBehaviour
+{
+    
+    private StudioEventEmitter eventEmitter;
+
+    private void Awake()
+    {
+        eventEmitter = GetComponent<StudioEventEmitter>();
+        if (eventEmitter == null)
+        {
+            Debug.LogError("На объекте отсутствует компонент StudioEventEmitter!");
+        }
+    }
+
+    //On click we playsound, just for now. Gonna update it future.
+    private void OnMouseDown()
+    {
+        if (eventEmitter != null)
+        {
+            eventEmitter.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/TriggerOtherSound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using FMODUnity;
+
+public class ClickTriggerSound : MonoBehaviour
+{
+    //Trigger other sound that is located somewhere else, when triggering some other object in front.
+    public StudioEventEmitter soundEmitter;
+
+    private void OnMouseDown()
+    {
+        if (soundEmitter != null)
+        {
+            soundEmitter.Play();
+        }
+        else
+        {
+            Debug.LogWarning("Ссылка на soundEmitter не назначена!");
+        }
+    }
+}
