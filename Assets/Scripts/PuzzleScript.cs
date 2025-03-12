4c773ede57d0e2cffd4d8dda653e1fc9573ae8a8
Apacado26 <144462653+Apacado26@users.noreply.github.com>
2025-03-12 10:40:48 -0400

    Nearly Finalized Puzzle 1

--- Assets/Scripts/PuzzleScript.cs
+++ Assets/Scripts/PuzzleScript.cs
@@ -1,9 +1,35 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PuzzleScript : MonoBehaviour
 {
-    
+    [Header("Locked Door")]
+    [SerializeField] private GameObject lockedDoor;
+
+    private bool unlockDoor = false;
+    public ButtonSwapTexture[] buttons;
+    private int targetValue = 5;
+
+    public void CheckCombination()
+    {
+        unlockDoor = true;
+        foreach (ButtonSwapTexture button in buttons)
+        {
+            if (button.GetCurrentIndex() != targetValue)
+            {
+                unlockDoor = false;
+                break;
+            }
+        }
+        if(lockedDoor != null && unlockDoor == true)
+        {
+            Debug.Log("Locked Door is not null");
+            lockedDoor.gameObject.SetActive(false);
+        }
+    }
 }
