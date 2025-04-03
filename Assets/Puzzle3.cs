1b9bd71556bc324e6050a29340cdeb2466f72fa3
Apacado26 <144462653+Apacado26@users.noreply.github.com>
2025-04-03 16:49:01 -0400

    Incorporated notes to game scene and fixed Puzzle 3

--- Assets/Puzzle3.cs
+++ Assets/Puzzle3.cs
@@ -72,6 +72,7 @@ public class Puzzle3 : MonoBehaviour
                 if (IsSameRow(button, selectedButton))
                 {
                     SwapLetters(button, selectedButton);
+
                     CheckPuzzleSolved();
                     selectedButton.GetComponent<Image>().color = Color.white;
                     button.GetComponent<Image>().color = Color.white;
@@ -133,9 +134,13 @@ public class Puzzle3 : MonoBehaviour
         string currentOliver = GetConcatenatedText(oliverButtons);
         string currentWendy = GetConcatenatedText(wendyButtons);
 
+        Debug.Log("Oliver Row: " + currentOliver);
+        Debug.Log("Wendy Row: " + currentWendy);
+
 
         if (currentOliver == correctOliver && currentWendy == correctWendy)
         {
+            Debug.Log("Attempting to open door");
             BasementDoor.isLocked = false;
         }
     }
